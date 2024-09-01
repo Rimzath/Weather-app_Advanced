07079c01e4b4b7da1e25d00fd0dabe3fb873928e
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2024-09-01 18:14:41 +0530

    Initial comment

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    dependencies {
+        // Add the Google Services Gradle plugin classpath
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+}
